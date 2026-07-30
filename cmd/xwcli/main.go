09ec698c90277f2561ebb0a568481c1cf08da12d
xiaodongQ <xiaodong0795@163.com>
2026-07-30 23:39:14 +0800

    fix(xwcli): register 命令参数解析失败 - 先提子命令再解析剩余 args

--- cmd/xwcli/main.go
+++ cmd/xwcli/main.go
@@ -8,60 +8,56 @@ import (
 )
 
 var (
-	flagServer  string
-	flagName    string
-	flagVersion bool
+	flagServer string
+	flagName   string
 )
 
 func main() {
-	fs := flag.NewFlagSet("xwcli", flag.ContinueOnError)
-	fs.StringVar(&flagServer, "server", "", "xworkbench server URL (e.g. http://localhost:8902)")
-	fs.StringVar(&flagName, "name", hostname(), "machine name for this agent")
-	fs.BoolVar(&flagVersion, "version", false, "print version")
-
-	if err := fs.Parse(os.Args[1:]); err != nil {
-		if err == flag.ErrHelp {
-			printUsage(fs)
-			os.Exit(0)
-		}
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	if len(os.Args) < 2 {
+		printUsage(nil)
 		os.Exit(1)
 	}
 
-	if flagVersion {
-		fmt.Println("xwcli 1.0.0")
-		return
-	}
+	cmd := os.Args[1]
+	args := os.Args[2:]
 
-	if fs.NArg() < 1 {
-		printUsage(fs)
-		os.Exit(1)
-	}
-
-	cmd := fs.Arg(0)
-
-	var err error
 	switch cmd {
 	case "register":
-		err = cmdRegister()
+		fs := flag.NewFlagSet("register", flag.ExitOnError)
+		fs.StringVar(&flagServer, "server", "", "xworkbench server URL (e.g. http://localhost:8902)")
+		fs.StringVar(&flagName, "name", hostname(), "machine name for this agent")
+		if err := fs.Parse(args); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		if err := cmdRegister(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "run":
-		err = cmdRun()
+		if err := cmdRun(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "status":
-		err = cmdStatus()
+		if err := cmdStatus(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
 	case "stop":
-		err = cmdStop()
+		if err := cmdStop(); err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+	case "version", "--version", "-version":
+		fmt.Println("xwcli 1.0.0")
 	default:
 		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
-		printUsage(fs)
-		os.Exit(1)
-	}
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func printUsage(fs *flag.FlagSet) {
+func printUsage(_ *flag.FlagSet) {
 	fmt.Fprintf(os.Stderr, `xwcli - xworkbench 远程 agent 客户端
 
 用法:
@@ -71,8 +67,9 @@ func printUsage(fs *flag.FlagSet) {
     xwcli stop
 
 选项:
+  --server string    xworkbench server URL (register 子命令)
+  --name string      机器名称 (register 子命令，默认本机 hostname)
 `)
-	fs.PrintDefaults()
 }
 
 // hostname returns the machine hostname, falling back to "unknown".
